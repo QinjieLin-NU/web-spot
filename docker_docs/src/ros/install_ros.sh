9ee436ded1be55529c1640b06ae3b9d342dcebb2
qinjielin-nu <qinjielin2018@u.northwestern.edu>
2021-06-30 23:25:53 +0000

    add docker

--- docker_docs/src/ros/install_ros.sh
+++ docker_docs/src/ros/install_ros.sh
@@ -2,3 +2,9 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 apt update -y
 apt install -y ros-melodic-desktop-full
+
+#install dependency
+apt install -y ros-melodic-joint-state-publisher-gui ros-melodic-interactive-marker-twist-server ros-melodic-robot-state-publisher
+apt-get install -y ros-melodic-rosbridge-suite
+apt-get install -y python-pip
+pip install  rosdep
--- docker_docs/src/ros/install_spot.sh
+++ docker_docs/src/ros/install_spot.sh
@@ -1,20 +1,14 @@
-#install dependency
-apt install -y ros-melodic-joint-state-publisher-gui ros-melodic-interactive-marker-twist-server ros-melodic-robot-state-publisher
-apt-get install -y ros-melodic-rosbridge-suite
-
 #catkin_make packages
 cd /root
 mkdir spot_ws
 mkdir spot_ws/src
 cd spot_ws/src
 git clone https://github.com/QinjieLin-NU/web-spot.git
-cd /root/spot_ws
-apt-get install -y python-pip
-pip install  rosdep
-source /opt/ros/melodic/setup.bash && \
-rosdep init && \
-rosdep update && \
-rosdep install --from-paths src --ignore-src -r -y 
-source /opt/ros/melodic/setup.bash && \
-catkin_make
+# cd /root/spot_ws
+# sh /opt/ros/melodic/setup.bash && \
+# rosdep init && \
+# rosdep update && \
+# rosdep install --from-paths src --ignore-src -r -y 
+# sh /opt/ros/melodic/setup.bash && \
+# catkin_make
 
